9f0e022840e59a0f810b1107cfc8940567a8ca64
Valerian Coelho <valerian27012004@gmail.com>
2025-03-14 20:38:25 +0530

    FullScreen VIdeo Loaded

--- app/index.tsx
+++ app/index.tsx
@@ -1,91 +1,36 @@
-import { View, Dimensions, FlatList, StyleSheet, Pressable } from 'react-native';
-import { useEffect, useRef, useState } from 'react';
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { useEvent } from 'expo';
-
-const { height, width } = Dimensions.get('window');
+import { useVideoPlayer, VideoView, VideoSource } from "expo-video";
+import { StyleSheet, View } from "react-native";
 
 const videos = [
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
-  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
 ];
 
-export default function Index() {
-  const [currentViewableItemIndex, setCurrentViewableItemIndex] = useState(0);
-  const viewabilityConfig = { viewAreaCoveragePercentThreshold: 50 };
+const bigBuckBunnySource: VideoSource = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
-  const onViewableItemsChanged = ({ viewableItems }) => {
-    if (viewableItems.length > 0) {
-      setCurrentViewableItemIndex(viewableItems[0].index ?? 0);
-    }
-  };
-
-  const viewabilityConfigCallbackPairs = useRef([{ viewabilityConfig, onViewableItemsChanged }]);
+export default function PreloadingVideoPlayerScreen() {
+  const player1 = useVideoPlayer(bigBuckBunnySource, (player) => {
+    player.play();
+  });
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={videos}
-        renderItem={({ item, index }) => (
-          <Item item={item} shouldPlay={index === currentViewableItemIndex} />
-        )}
-        keyExtractor={(item) => item}
-        pagingEnabled
-        // horizontal={false}
-        // showsVerticalScrollIndicator={false}
-        viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
-        // initialNumToRender={2} // Render 3 items initially
-        windowSize={3} // Keep 3 items in memory
-        // maxToRenderPerBatch={2} // Render 3 at a time
-        // removeClippedSubviews={true} // Unmount off-screen items
-        // snapToInterval={height} // Snap to full-screen video
-        snapToAlignment="start"
-        decelerationRate="fast" // Faster scrolling like Instagram
-      />
+    <View style={styles.contentContainer}>
+      <VideoView player={player1} style={styles.video} contentFit="cover"/>
     </View>
   );
 }
 
-const Item = ({ item, shouldPlay }) => {
-  const player = useVideoPlayer(item, (player) => {
-    player.loop = true; // Enable looping
-  });
-
-  const { isPlaying } = useEvent(player, 'playingChange', {
-    isPlaying: player.playing,
-  });
-
-  useEffect(() => {
-    if (shouldPlay) {
-      player.play();
-    } else {
-      player.pause();
-      player.currentTime = 0; // Reset to beginning
-    }
-  }, [shouldPlay]);
-
-  return (
-    <Pressable onPress={() => (isPlaying ? player.pause() : player.play())}>
-      <View style={styles.videoContainer}>
-        <VideoView style={styles.video} player={player} contentFit='cover' />
-      </View>
-    </Pressable>
-  );
-};
-
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-  },
-  videoContainer: {
-    width,
-    height,
+    alignItems: "center",
+    justifyContent: "center",
   },
   video: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
 });
