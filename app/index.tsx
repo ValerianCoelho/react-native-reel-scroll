7f952bdef33e79856da7cead053089d7ffdfb2a8
Valerian Coelho <valerian27012004@gmail.com>
2025-03-22 15:15:03 +0530

    Scroll like insta complete

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,55 @@
 import Slider from "@react-native-community/slider";
 import { useEvent } from "expo";
-import { useVideoPlayer, VideoSource, VideoView } from "expo-video";
+import { useVideoPlayer, VideoPlayer, VideoSource, VideoView } from "expo-video";
 import React, { useEffect, useRef, useState } from "react";
 import { FlatList, Dimensions, StyleSheet, SafeAreaView, Text, View, Pressable } from "react-native";
 
 const videos = [
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
 ];
 
 const { width, height } = Dimensions.get("window");
 
-
 function Reel({ videoLink, shouldPlay }: { videoLink: VideoSource; shouldPlay: boolean }) {
   const player = useVideoPlayer(videoLink, (player) => {
-    player.loop = true;
+    player.loop = true; // Determines whether the player should automatically replay after reaching the end of the video.
+    player.bufferOptions = {
+      minBufferForPlayback: 1024 * 1024, // Android Only | Minimum duration of the buffer in seconds (default is 2) required to continue playing after the player has been paused or started buffering. This property will be ignored if preferredForwardBufferDuration is lower.
+      maxBufferBytes: 1024 * 1024, // Android Only | The maximum number of bytes that the player can buffer from the network. When 0 (default) the player will automatically decide appropriate buffer size.
+      preferredForwardBufferDuration: 1, // Android & iOS | The duration in seconds which determines how much media the player should buffer ahead of the current playback time. On iOS when set to 0 the player will automatically decide appropriate buffer duration. (Android: 20, iOS: 0)
+      // prioritizeTimeOverSizeThreshold: false, // Android | A Boolean value which determines whether the player should prioritize time over size when buffering media. (default: false)
+    }
+    
   });
+  useEffect(()=>{
+    console.log(player.status)
+  }, [player])
+  // useEffect(()=>{
+    // console.log(player.bufferedPosition)
+    // console.log(player.duration) // Float value indicating the duration of the current video in seconds.
+    // console.log(player.playing) // Boolean value whether the player is currently playing. Use play and pause methods to control the playback.
+    // console.log(player.status)  // Indicates the current status of the player.
+    //                             // idle: The player is not playing or loading any videos.
+    //                             // loading: The player is loading video data from the provided source
+    //                             // readyToPlay: The player has loaded enough data to start playing or to continue playback.
+    //                             // error: The player has encountered an error while loading or playing the video.
+
+    // player.currentTime = 0; // Float value indicating the current playback time in seconds. If the player is not yet playing, this value indicates the time position at which playback will begin once the play() method is called.
+    // player.muted = false; // Boolean value whether the player is currently muted. Setting this property to true/false will mute/unmute the player.
+    // player.replay() // Seeks the playback to the beginning.
+  // }, [])
 
   const { isPlaying } = useEvent(player, "playingChange", {
     isPlaying: player.playing,
+    
   });
 
   const [progress, setProgress] = useState(0);
   const [duration, setDuration] = useState(1); // Default to 1 to avoid division by zero
-
-  useEffect(() => {
-    console.log("Reel Mounted:", videoLink);
-
-    return () => {
-      console.log("Reel Unmounted:", videoLink);
-    };
-  }, []);
 
   useEffect(() => {
     if (shouldPlay) {
@@ -46,7 +62,7 @@ function Reel({ videoLink, shouldPlay }: { videoLink: VideoSource; shouldPlay: b
   useEffect(() => {
     const updateProgress = () => {
       setProgress(player.currentTime);
-      
+
       // Only update duration if it's a valid number and not zero
       if (player.duration && player.duration > 0) {
         setDuration(player.duration);
@@ -65,12 +81,13 @@ function Reel({ videoLink, shouldPlay }: { videoLink: VideoSource; shouldPlay: b
 
   return (
     <Pressable style={styles.reelContainer}>
-      <VideoView player={player} style={styles.reel} contentFit="cover" nativeControls={false} />
+      
+      <VideoView player={player} style={styles.reel} contentFit="cover" nativeControls={true} />
 
       <Pressable style={styles.overlayTouchable} onPress={() => (isPlaying ? player.pause() : player.play())}></Pressable>
 
       {/* Slider for Seeking */}
-      <View style={styles.sliderContainer}>
+      {/* <View style={styles.sliderContainer}>
         <Slider
           style={{ width: "90%", height: 40 }}
           minimumValue={0}
@@ -81,7 +98,7 @@ function Reel({ videoLink, shouldPlay }: { videoLink: VideoSource; shouldPlay: b
           maximumTrackTintColor="#888"
           thumbTintColor="#5C00CF"
         />
-      </View>
+      </View> */}
     </Pressable>
   );
 }
@@ -96,6 +113,7 @@ const FullScreenList = () => {
     }
   };
 
+  const ITEM_HEIGHT = Dimensions.get("window").height;
   const viewabilityConfigCallbackPairs = useRef([{ viewabilityConfig, onViewableItemsChanged }]);
 
   return (
@@ -109,10 +127,18 @@ const FullScreenList = () => {
         showsVerticalScrollIndicator={false}
         decelerationRate="fast"
         viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
-        initialNumToRender={3}
-        maxToRenderPerBatch={2}
+        initialNumToRender={2}
+        // maxToRenderPerBatch={2}
         windowSize={2}
+        snapToAlignment="center"
+        snapToInterval={ITEM_HEIGHT} // Replace with item height
+        getItemLayout={(data, index) => ({
+          length: ITEM_HEIGHT,
+          offset: ITEM_HEIGHT * index,
+          index,
+        })}
       />
+
     </SafeAreaView>
   );
 };
@@ -149,7 +175,7 @@ const styles = StyleSheet.create({
     width: "90%",
     alignSelf: "center",
   },
-   sliderContainer: {
+  sliderContainer: {
     position: "absolute",
     bottom: 20, // Adjust as needed
     left: 0,
